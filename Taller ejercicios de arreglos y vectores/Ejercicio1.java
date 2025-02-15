53c11fa932a3a78e5aced50b4448f8fe6cde9202
AlexanderUniajc <jalexandervelez@estudiante.uniajc.edu.co>
2025-02-15 23:48:55 +0000

    Suma de elementos del vector

---
+++ Taller ejercicios de arreglos y vectores/Ejercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejercicio1{
+    public static void main (String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Usuario ingrese el valor del vector:");
+        int n = sc.nextInt();
+
+        int [] vector = new int[n];
+        
+        System.out.println("Usuario ingrese los " + n + " 7valores");
+        for(int i=0; i<n; i++){
+            vector [i] = sc.nextInt();
+        }
+
+        int suma = 0;
+        for(int i=0; i<n; i++){
+            suma +=vector[i];
+        } 
+        System.out.println("La suma de los elementos del vector es: "+suma);   
+        sc.close();
+    }
+}
