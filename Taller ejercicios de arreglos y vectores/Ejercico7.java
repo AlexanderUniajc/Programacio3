9545075e5f54478d583687ac27de97111297ac7b
AlexanderUniajc <jalexandervelez@estudiante.uniajc.edu.co>
2025-02-16 16:50:11 +0000

    Ejercicio7 listo

--- Taller ejercicios de arreglos y vectores/Ejercico7.java
+++ Taller ejercicios de arreglos y vectores/Ejercico7.java
@@ -1,18 +1,25 @@
 public class Ejercico7 {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 5;
         int [] pares = new int[n];
         int [] inpares = new int[n];
 
         int numPar = 2;
-        int numInpar = 2;
+        int numInpar = 1;
 
         for(int i=0; i<n; i++){
             pares [i] = numPar;
             inpares [i] = numInpar;
             numPar += 2;
             numInpar += 2;
-
         }
+        System.out.println("Numneros pares");
+        for(int i=0; i<n; i++){
+            System.out.println(pares [i]);
+        }
+        System.out.println("Numneros inpares");
+        for(int i=0; i<n; i++){
+            System.out.println(inpares [i]);
     }
+}
 }
